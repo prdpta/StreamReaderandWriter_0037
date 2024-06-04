f5753fc798ec6c9a06d950300b23e854cb41f194
prdpta <pradiptaputramei01@gmail.com>
2024-06-04 17:55:03 +0700

    menambahkan while true

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -16,3 +16,24 @@ int main() {
   outfile.open(NamaFile + ".txt", ios::out);
 
   cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+  // unlimited loop untuk menulis
+  while (true) {
+    cout << "- ";
+    //mendapatkan setiap karakter dalam satu baris
+    getline(cin, baris);
+    //loop akan berhenti jika anda memasukan karakter q
+    if (baris == "q") break;
+    // menulis dan memasukan nilai dari 'baris' ke dalam file
+    outfile << baris << endl;
+  }
+  // selesai dalam menulis sekarang tutup filenya
+  outfile.close();
+
+  // Membuka file dalam mode membaca
+  ifstream infile;
+
+  // menunjukan ke sebuah file
+  infile.open(NamaFile + ".txt", ios::in); 
+
+  cout << endl << ">= Membuka dan membaca file " << endl;
