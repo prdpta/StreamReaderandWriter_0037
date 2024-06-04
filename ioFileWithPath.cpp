7e8d75d255976e4844567668de4696136cfe02d6
prdpta <pradiptaputramei01@gmail.com>
2024-06-04 17:56:04 +0700

    menambahkan if

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -37,3 +37,21 @@ int main() {
   infile.open(NamaFile + ".txt", ios::in); 
 
   cout << endl << ">= Membuka dan membaca file " << endl;
+  // jika file ada maka
+  if (infile.is_open())
+  {
+
+    // melakukan perulangan setiap baris
+    while (getline(infile, baris))
+    {
+
+      //dan tampilkan di sini
+      cout << baris << '\n';
+    }
+    // tutup file tersebut setelah selesai
+    infile.close();
+  }
+  //jika tidak ditemukan file maka akan menampilkan ini
+  else cout << "Unable to open file";
+  return 0;
+}
